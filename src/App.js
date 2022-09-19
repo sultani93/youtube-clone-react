48ccf511e03e3f3cb74b0fe4599cf36a6f990f26
Sultani Sanan <54854030+sultani93@users.noreply.github.com>
2022-09-19 21:55:20 +0400

    Search Bar component designed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Box } from '@mui/material'
+import {
+  Navbar,
+  Feed,
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
+} from './components'
 
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route path='/' exact element={<Feed />}></Route>
-        <Route path='/video/:id' element={<VideoDetail />}></Route>{' '}
-        <Route path='/channel/:id' element={<ChannelDetail />}></Route>{' '}
-        <Route path='/search/:searchTerm' element={<SearchFeed />}></Route>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
